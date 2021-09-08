763b9ea0b9dd38bc513b0c7bcb64360353adbd67
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-09-08 10:38:59 +0200

    :lipstick: blur section background

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Intro = () => (
-    <div className="z-10 flex flex-col gap-6 text-lg">
+    <div className="z-10 flex flex-col gap-6 text-lg backdrop-filter backdrop-blur-sm">
         <p>Avec plus de 6 ans d'expérience en développement, j'ai été amené à évoluer dans des contextes riches et variés.</p>
         <p>Du backend au frontend, j'aime répondre à des problématiques complexes, en concevant puis en développant les solutions les plus simples possible.</p>
         <p>En parallèle, je suis également formateur sur les technologies que j'utilise au quotidien : Git, React, Java, JavaScript, etc.</p>
--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import * as React from "react"
 import SectionTitle from "./SectionTitle"
 
 const Section = ({ title, children }) => (
-    <div className="z-10 my-10">
+    <div className="z-10 my-10 backdrop-filter backdrop-blur-sm">
         <SectionTitle {...title} />
         {children}
     </div>
