0b7a1c1dc1708a2e4e09a3fe0c2ce86a9ad713b9
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-09-10 08:58:30 +0200

    :lipstick: title illustration size

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 
 const SectionTitle = ({ value, illustration }) => (
     <div className="w-full flex flex-col items-center mt-12 mb-8">
-        <img className="w-1/4" src={illustration} alt="" />
+        <img className="mx-auto max-w-xs w-full" src={illustration} alt="" />
         <h3 className="text-4xl text-primary-800 capitalize -mt-3">{value}</h3>
     </div>
 )
