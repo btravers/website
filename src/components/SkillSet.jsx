b91aa7314867017026fe6cd408bbcc62e71da054
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-09-17 10:24:19 +0200

    improve accessibility

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -5,7 +5,7 @@ import Skill from "./Skill"
 
 const SKillSet = ({ name, skills }) => (
     <div>
-        <h4 className="text-lg mb-1 text-neutral">{name}</h4>
+        <h4 className="text-lg mb-1 text-gray-500">{name}</h4>
         <div className="divide-y divide-gray-200 divide-solid">
             {skills.sort(({ mark: m1 }, { mark: m2 }) => m2 - m1).map(({ name: skill, mark }) => <Skill key={skill} name={skill} mark={mark} />)}
         </div>
