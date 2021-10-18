f1898ec26593c3ab989497f11b93b988d587ad58
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-10-18 15:31:49 +0200

    refactor: merge intro parts

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -10,11 +10,12 @@ const IntroSection = () => {
     return (
         <Section>
             <div
-                className="max-w-6xl mx-auto w-full xl:divide-x-2 grid gap-8 xl:grid-cols-3 items-center mx-auto my-10 px-5">
-                <div className="mx-auto">
+                className="max-w-6xl mx-auto w-full xl:divide-x-2 grid gap-8 xl:grid-cols-3 items-center mx-auto my-10 px-5 print:text-gray-700 print:my-0 print:px-0"
+            >
+                <div className="mx-auto print:hidden">
                     <Portrait/>
                 </div>
-                <div className="text-lg xl:col-span-2 xl:pl-8">
+                <div className="text-lg xl:col-span-2 xl:pl-8 print:text-base">
                     <CustomizedMDXRenderer>
                         {intro.body}
                     </CustomizedMDXRenderer>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,6 @@ import Portrait from "../components/Portrait"
 import ContactPart from "../components/pdf/ContactPart"
 import EducationPart from "../components/pdf/EducationPart"
 import TitlePart from "../components/pdf/TitlePart"
-import IntroPart from "../components/pdf/IntroPart"
 import WorkExperiencePart from "../components/pdf/WorkExperiencePart"
 
 const IndexPage = () => (
@@ -22,7 +21,6 @@ const IndexPage = () => (
         <SEO/>
         <div className="bg-grey-50 text-primary-800 print:hidden">
             <TitleSection/>
-            <IntroSection/>
             <SkillsSection/>
             <WorkExperienceSection/>
             <EducationSection/>
@@ -40,9 +38,9 @@ const IndexPage = () => (
                         <EducationPart/>
                         <SkillsSection/>
                     </div>
-                    <div className="col-span-2 flex flex-col gap-16 text-primary-800 px-12 py-10">
+                    <div className="col-span-2 flex flex-col gap-8 text-primary-800 px-12 py-10">
                         <TitlePart/>
-                        <IntroPart/>
+                        <IntroSection/>
                         <WorkExperiencePart offset={0} size={3}/>
                     </div>
                 </div>
