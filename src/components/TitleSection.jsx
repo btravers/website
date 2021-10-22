27ae0d39dd432220ffc87f5feda2841e378b4f24
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-10-22 09:13:40 +0200

    chore: update dependencies

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -13,8 +13,7 @@ const TitleSection = () => (
     <Section dark>
         <div className="mx-auto w-full flex flex-col">
             <Header/>
-            <div
-                className="max-w-8xl flex-grow mx-auto grid grid-cols-1 xl:grid-cols-2 items-center print:grid-cols-1 print:items-start print:mx-0">
+            <div className="max-w-8xl flex-grow mx-auto grid grid-cols-1 xl:grid-cols-2 items-center print:grid-cols-1 print:items-start print:mx-0">
                 <div className="flex flex-col items-center justify-center print:items-start">
                     <img className="mx-3 md:mx-5 lg:mx-10 xl:mx-0 mb-8 max-w-2xl print:hidden" src={logo} alt=""/>
                     <h1 className="text-6xl uppercase hidden print:block">
