e114bbe4b1ddad38389a9e0b38bb6f6c0bcba5bd
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-10-22 09:26:05 +0200

    style: mobile title

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -14,29 +14,33 @@ const TitleSection = () => (
         <div className="mx-auto w-full flex flex-col">
             <Header/>
             <div className="max-w-8xl flex-grow mx-auto grid grid-cols-1 xl:grid-cols-2 items-center print:grid-cols-1 print:items-start print:mx-0">
-                <div className="flex flex-col items-center justify-center print:items-start">
-                    <img className="mx-3 md:mx-5 lg:mx-10 xl:mx-0 mb-8 max-w-2xl print:hidden" src={logo} alt=""/>
-                    <h1 className="text-6xl uppercase hidden print:block">
-                        <span className="font-bold">Benoit</span> Travers
-                    </h1>
-                    <div className="text-xl md:text-3xl text-center my-2 print:text-2xl print:uppercase">
-                        <h2>développeur full stack freelance</h2>
-                        <div className="print:hidden">basé à Rennes, France</div>
-                    </div>
-                    <div className="inline-grid grid-cols-2 my-6 gap-5 print:hidden">
-                        <a href="https://www.linkedin.com/in/btraversfr/" target="_blank" rel="noopener noreferrer"
-                           aria-label="linkedin">
-                            <LinkedIn/>
-                        </a>
-                        <a href="https://www.github.com/btravers/" target="_blank" rel="noopener noreferrer"
-                           aria-label="github">
-                            <GitHub/>
-                        </a>
-                    </div>
-                </div>
                 <div>
+                    <Animated animation="fadeIn">
+                        <div className="flex flex-col items-center justify-center print:items-start">
+                            <img className="mx-3 md:mx-5 lg:mx-10 xl:mx-0 mb-8 max-w-2xl print:hidden" src={logo} alt=""/>
+                            <h1 className="text-6xl uppercase hidden print:block">
+                                <span className="font-bold">Benoit</span> Travers
+                            </h1>
+                            <div className="text-xl md:text-3xl text-center my-2 print:text-2xl print:uppercase">
+                                <h2>développeur full stack freelance</h2>
+                                <div className="print:hidden">basé à Rennes, France</div>
+                            </div>
+                            <div className="inline-grid grid-cols-2 my-6 gap-5 print:hidden">
+                                <a href="https://www.linkedin.com/in/btraversfr/" target="_blank" rel="noopener noreferrer"
+                                   aria-label="linkedin">
+                                    <LinkedIn/>
+                                </a>
+                                <a href="https://www.github.com/btravers/" target="_blank" rel="noopener noreferrer"
+                                   aria-label="github">
+                                    <GitHub/>
+                                </a>
+                            </div>
+                        </div>
+                    </Animated>
+                </div>
+                <div className="hidden xl:block print:hidden">
                     <Animated animation="fadeInRight">
-                        <img className="hidden xl:block print:hidden" src={workingGuyIllustration} alt=""/>
+                        <img src={workingGuyIllustration} alt=""/>
                     </Animated>
                 </div>
             </div>
