7e8f73d77f3fbfddc4945a2ce7e0106a175e57de
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2021-10-24 20:41:07 +0200

    feat: hide print layout

--- src/components/PrintLayout.jsx
+++ src/components/PrintLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 
 import Page from "./Page"
 import Portrait from "./Portrait"
@@ -9,32 +9,48 @@ import TitleSection from "./TitleSection"
 import IntroSection from "./IntroSection"
 import WorkExperienceSection from "./WorkExperienceSection"
 
-const PrintLayout = () => (
-    <div className="hidden print:block">
-        <Page first>
-            <div className="grid grid-cols-3 items-stretch h-full">
-                <div className="flex flex-col gap-3 pl-16 dark-print">
-                    <div className="my-10">
-                        <Portrait/>
+const PrintLayout = () => {
+    const [rendered, setRendered] = useState(false)
+
+    const isBrowser = typeof window !== "undefined"
+
+    if (isBrowser) {
+        window.onbeforeprint = () => {
+            setRendered(true)
+        }
+    }
+
+    if (!rendered) {
+        return (<></>)
+    }
+
+    return (
+        <div className="hidden print:block">
+            <Page first>
+                <div className="grid grid-cols-3 items-stretch h-full">
+                    <div className="flex flex-col gap-3 pl-16 dark-print">
+                        <div className="my-10">
+                            <Portrait/>
+                        </div>
+                        <ContactSection/>
+                        <EducationSection/>
+                        <SkillsSection/>
                     </div>
-                    <ContactSection/>
-                    <EducationSection/>
-                    <SkillsSection/>
+                    <div className="col-span-2 flex flex-col gap-4 text-color-1-800 px-12 py-8">
+                        <TitleSection/>
+                        <IntroSection/>
+                        <WorkExperienceSection offset={0} size={3}/>
+                    </div>
                 </div>
-                <div className="col-span-2 flex flex-col gap-4 text-color-1-800 px-12 py-8">
-                    <TitleSection/>
-                    <IntroSection/>
-                    <WorkExperienceSection offset={0} size={3}/>
-                </div>
-            </div>
-        </Page>
-        <Page margin>
-            <WorkExperienceSection offset={3} size={3}/>
-        </Page>
-        <Page margin>
-            <WorkExperienceSection offset={6}/>
-        </Page>
-    </div>
-)
+            </Page>
+            <Page margin>
+                <WorkExperienceSection offset={3} size={3}/>
+            </Page>
+            <Page margin>
+                <WorkExperienceSection offset={6}/>
+            </Page>
+        </div>
+    )
+}
 
 export default PrintLayout
